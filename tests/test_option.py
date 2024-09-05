ae7f033c6766daab57e86cb988487ddd2ade6234
iceice666 <iceice666@outlook.com>
2024-09-06 03:51:41 +0800

    Test: add pytest for test

---
+++ tests/test_option.py
@@ -0,0 +1,103 @@
+import pytest
+
+from src.common import UnwrapError
+from src.option import Option
+from src.result import Result
+
+
+def test_is_some() -> None:
+    assert Option(42).is_some() is True
+    assert Option().is_some() is False
+
+
+def test_is_none() -> None:
+    assert Option(42).is_none() is False
+    assert Option().is_none() is True
+
+
+def test_is_some_and() -> None:
+    assert Option(42).is_some_and(lambda x: x > 0) is True
+    assert Option(-1).is_some_and(lambda x: x > 0) is False
+    assert Option().is_some_and(lambda x: x > 0) is False
+
+
+def test_expect() -> None:
+    assert Option(42).expect("Should not happen") == 42
+    with pytest.raises(ValueError):
+        Option().expect("Should happen")
+
+
+def test_unwrap() -> None:
+    assert Option(42).unwrap() == 42
+    with pytest.raises(UnwrapError):
+        Option().unwrap()
+
+
+def test_unwrap_or() -> None:
+    assert Option(42).unwrap_or(0) == 42
+    assert Option().unwrap_or(0) == 0
+
+
+def test_unwrap_or_else() -> None:
+    assert Option(42).unwrap_or_else(lambda: 0) == 42
+    assert Option().unwrap_or_else(lambda: 0) == 0
+
+
+def test_map() -> None:
+    assert Option(42).map(lambda x: x * 2) == Option(84)
+    assert Option().map(lambda x: x * 2) == Option()
+
+
+def test_map_or() -> None:
+    assert Option(42).map_or(0, lambda x: x * 2) == 84
+    assert Option().map_or(0, lambda x: x * 2) == 0
+
+
+def test_map_or_else() -> None:
+    assert Option(42).map_or_else(lambda: 0, lambda x: x * 2) == 84
+    assert Option().map_or_else(lambda: 0, lambda x: x * 2) == 0
+
+
+def test_inspect() -> None:
+    def side_effect(_: object) -> None:
+        pass
+
+    assert Option(42).inspect(side_effect) == Option(42)
+    assert Option().inspect(side_effect) == Option()
+
+
+def test_ok_or() -> None:
+    assert Option(42).ok_or(ValueError("Error")) == Result(ok=42)
+    assert Option().ok_or(ValueError("Error")) == Result(err=ValueError("Error"))
+
+
+def test_ok_or_else() -> None:
+    assert Option(42).ok_or_else(lambda: ValueError("Error")) == Result(ok=42)
+    assert Option().ok_or_else(lambda: ValueError("Error")) == Result(err=ValueError("Error"))
+
+
+def test_and_() -> None:
+    assert Option(42).and_(Option("foo")) == Option("foo")
+    assert Option().and_(Option("foo")) == Option()
+
+
+def test_and_then() -> None:
+    assert Option(42).and_then(lambda x: Option(x * 2)) == Option(84)
+    assert Option().and_then(lambda x: Option(x * 2)) == Option()
+
+
+def test_or_() -> None:
+    assert Option(42).or_(Option(0)) == Option(42)
+    assert Option().or_(Option(0)) == Option(0)
+
+
+def test_or_else() -> None:
+    assert Option(42).or_else(lambda: Option(0)) == Option(42)
+    assert Option().or_else(lambda: Option(0)) == Option(0)
+
+
+def test_xor() -> None:
+    assert Option(42).xor(Option()) == Option(42)
+    assert Option().xor(Option(42)) == Option(42)
+    assert Option(42).xor(Option(42)) == Option()
+    assert Option().xor(Option()) == Option()
---
+++ tests/test_result.py
@@ -0,0 +1,149 @@
+import pytest
+
+from src.common import UnwrapError
+from src.result import Result
+
+
+class TestException(Exception):
+    __test__ = False
+
+    def __eq__(self, other: object) -> bool:
+        return isinstance(other, TestException)
+
+
+def get_exception(msg: str = "Error occurred") -> TestException:
+    return TestException(msg)
+
+
+ResT = Result[int, TestException]
+
+
+def test_ok_value() -> None:
+    result: ResT = Result(ok=42)
+    assert result.is_ok() is True
+    assert result.is_err() is False
+    assert result.ok_value == 42
+
+
+def test_err_value() -> None:
+    result: ResT = Result(err=get_exception())
+    assert result.is_ok() is False
+    assert result.is_err() is True
+    assert result.err_value == get_exception()
+
+
+def test_invalid_initialization() -> None:
+    with pytest.raises(ValueError):
+        Result(ok=42, err=get_exception())
+    with pytest.raises(ValueError):
+        Result()
+
+
+def test_ok_method() -> None:
+    result: ResT = Result(ok=42)
+    assert result.ok().is_some() is True
+    assert result.ok().unwrap() == 42
+
+
+def test_err_method() -> None:
+    result: ResT = Result(err=get_exception())
+    assert result.err().is_some() is True
+    assert result.err().unwrap() == get_exception()
+
+
+def test_map_ok_value() -> None:
+    result: ResT = Result(ok=2)
+    new_result = result.map(lambda x: x * 2)
+    assert new_result.is_ok() is True
+    assert new_result.ok_value == 4
+
+
+def test_map_err_value() -> None:
+    result: ResT = Result(err=get_exception())
+    new_result = result.map(lambda x: x * 2)
+    assert new_result.is_err() is True
+    assert new_result.err_value == get_exception()
+
+
+def test_map_err() -> None:
+    def map_err(_: Exception) -> Exception:
+        return get_exception("Error occurred again")
+
+    result: ResT = Result(err=get_exception())
+    new_result = result.map_err(map_err)
+    assert new_result.is_err() is True
+    assert new_result.err_value == get_exception("Error occurred again")
+
+
+def test_map_or() -> None:
+    result_ok: ResT = Result(ok=10)
+    result_err: ResT = Result(err=get_exception())
+    assert result_ok.map_or(5, lambda x: x * 2) == 20
+    assert result_err.map_or(5, lambda x: x * 2) == 5
+
+
+def test_map_or_else() -> None:
+    result_ok: ResT = Result(ok=10)
+    result_err: ResT = Result(err=get_exception())
+    assert result_ok.map_or_else(lambda e: 5, lambda x: x * 2) == 20
+    assert result_err.map_or_else(lambda e: 5, lambda x: x * 2) == 5
+
+
+def test_unwrap_ok() -> None:
+    result: ResT = Result(ok=42)
+    assert result.unwrap() == 42
+
+
+def test_unwrap_err() -> None:
+    result: ResT = Result(err=get_exception())
+    with pytest.raises(UnwrapError):
+        result.unwrap()
+
+
+def test_unwrap_or() -> None:
+    result_ok: ResT = Result(ok=42)
+    result_err: ResT = Result(err=get_exception())
+    assert result_ok.unwrap_or(100) == 42
+    assert result_err.unwrap_or(100) == 100
+
+
+def test_unwrap_or_else() -> None:
+    result_ok: ResT = Result(ok=42)
+    result_err: ResT = Result(err=get_exception())
+    assert result_ok.unwrap_or_else(lambda e: 100) == 42
+    assert result_err.unwrap_or_else(lambda e: 100) == 100
+
+
+def test_expect() -> None:
+    result: ResT = Result(ok=42)
+    assert result.expect("Should not raise error") == 42
+
+
+def test_expect_err() -> None:
+    result: ResT = Result(err=get_exception())
+    with pytest.raises(UnwrapError):
+        result.expect("Error occurred")
+
+
+def test_and_ok() -> None:
+    result1: ResT = Result(ok=10)
+    result2: ResT = Result(ok=20)
+    assert result1.and_(result2).unwrap() == 20
+
+
+def test_and_err() -> None:
+    result1: ResT = Result(err=get_exception())
+    result2: ResT = Result(ok=20)
+    assert result1.and_(result2).is_err() is True
+
+
+def test_or_ok() -> None:
+    result1: ResT = Result(ok=10)
+    result2: ResT = Result(ok=20)
+    assert result1.or_(result2).unwrap() == 10
+
+
+def test_or_err() -> None:
+    result1: ResT = Result(err=get_exception())
+    result2: ResT = Result(ok=20)
+    assert result1.or_(result2).unwrap() == 20
